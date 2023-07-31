9d36b076685c7732d67d8e3c1d81631356d4d7a5
PRich57 <pcrichards57@gmail.com>
2023-07-31 17:53:25 -0600

    Add JS to project

---
+++ assets/script.js
@@ -0,0 +1,19 @@
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+
+for (let i = 0; i < topics.length; i++) {
+    console.log(topics[i]);
+}
+
+/*
+if (topic === 'HTML') {
+ console.log("Let's study HTML!");
+} else if (topic === 'CSS') {
+ console.log("Let's study CSS!");
+} else if (topic === 'Git') {
+ console.log("Let's study Git!");
+} else if (topic === 'JavaScript') {
+ console.log("Let's study JavaScript!");
+} else {
+ console.log('Please try again!');
+}
+*/
