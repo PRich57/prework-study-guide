3b6a735896a91d247a734cab2eee2c2fbe546848
PRich57 <pcrichards57@gmail.com>
2023-07-31 18:37:32 -0600

    Add interactivity via functions & loops with JS

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,27 @@
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
-for (let i = 0; i < topics.length; i++) {
-    console.log(topics[i]);
+function listTopics() {
+    for (let i = 0; i < topics.length; i++) {
+        console.log(topics[i]);
+    }
 }
 
-/*
-if (topic === 'HTML') {
- console.log("Let's study HTML!");
-} else if (topic === 'CSS') {
- console.log("Let's study CSS!");
-} else if (topic === 'Git') {
- console.log("Let's study Git!");
-} else if (topic === 'JavaScript') {
- console.log("Let's study JavaScript!");
-} else {
- console.log('Please try again!');
+function selectTopic() {
+    if (randomTopic === 'HTML') {
+        console.log("Let's study HTML!");
+    } else if (randomTopic === 'CSS') {
+        console.log("Let's study CSS!");
+    } else if (randomTopic === 'Git') {
+        console.log("Let's study Git!");
+    } else if (randomTopic === 'JavaScript') {
+        console.log("Let's study JavaScript!");
+    } else {
+        console.log('Please try again!');
+    }
 }
-*/
+
+console.log('Here are the topics we learned through Prework:');
+listTopics();
+console.log('Which topic should we study first?');
+selectTopic();
